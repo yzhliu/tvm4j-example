6c4ec1917bc052ad6f74a893cf1492a10f40180e
Yizhi Liu <yizhiliu@amazon.com>
2018-07-16 17:00:43 -0700

    LoadAddFunc example

---
+++ bin/add_func.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CURR_DIR=$(cd $(dirname $0); pwd)
+PROJ_DIR=$(cd $(dirname $0)/../; pwd)
+mkdir -p $CURR_DIR/libs
+
+echo "Run python code ..."
+python add_func.py $CURR_DIR/libs
+
+echo "Run java code ..."
+CLASSPATH=$CLASSPATH:$PROJ_DIR/target/*:$PROJ_DIR/target/classes/lib/*
+java -cp $CLASSPATH \
+  -Dlog4j.configuration=file://$PROJ_DIR/conf/log4j.properties \
+  me.yzhi.tvm4j.example.LoadAddFunc $CURR_DIR/libs
---
+++ src/main/java/me/yzhi/tvm4j/example/LoadAddFunc.java
@@ -0,0 +1,29 @@
+package me.yzhi.tvm4j.example;
+
+import ml.dmlc.tvm.Module;
+import ml.dmlc.tvm.NDArray;
+import ml.dmlc.tvm.TVMContext;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class LoadAddFunc {
+  public static void main(String[] args) {
+    String loadingDir = args[0];
+    Module fadd = Module.load(loadingDir + File.separator + "add_cpu.so");
+
+    TVMContext ctx = TVMContext.cpu();
+
+    long[] shape = new long[]{2};
+    NDArray arr = NDArray.empty(shape, ctx);
+    arr.copyFrom(new float[]{3f, 4f});
+    NDArray res = NDArray.empty(shape, ctx);
+
+    fadd.entryFunc().pushArg(arr).pushArg(arr).pushArg(res).invoke();
+    System.out.println(Arrays.toString(res.asFloatArray()));
+
+    arr.release();
+    res.release();
+    fadd.release();
+  }
+}
